82a4a4388ca2e16c2ca0f5f558ae5e718ebe8c1e
4Nanai <103087652@qq.com>
2026-03-30 16:12:41 -0400

    Upd: vscode unique keymap

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -21,29 +21,41 @@ end
 -- vscode keymaps
 if is_vscode then
 	local vscode = require("vscode")
-	local function vsc(lhs, action, desc)
+	local function vsc_normal(lhs, action, desc)
 		vim.keymap.set("n", lhs, function()
 			vscode.action(action)
 		end, { desc = desc })
 	end
-	vsc("<leader>bh", "workbench.action.previousEditor", "Prev Editor")
-	vsc("<leader>bl", "workbench.action.nextEditor", "Next Editor")
-	vsc("<leader>bp", "workbench.action.quickOpen", "Quick Open (Pick)")
-	vsc("<leader>bd", "workbench.action.closeActiveEditor", "Close Editor")
+	local function vsc_insert(lsh, action)
+		vim.keymap.set("i", lsh, function()
+			vscode.action(action)
+		end)
+	end
+	local function vsc_mode(mode, lsh, action)
+		vim.keymap.set(mode, lsh, function()
+			vscode.action(action)
+		end)
+	end
+	vsc_normal("<leader>bh", "workbench.action.previousEditor", "Prev Editor")
+	vsc_normal("<leader>bl", "workbench.action.nextEditor", "Next Editor")
+	vsc_normal("<leader>bp", "workbench.action.quickOpen", "Quick Open (Pick)")
+	vsc_normal("<leader>bd", "workbench.action.closeActiveEditor", "Close Editor")
 
 	-- Split View Controls (VS Code Actions)
-	vsc("<leader>wv", "workbench.action.splitEditorRight", "Split Vertical")
-	vsc("<leader>ws", "workbench.action.splitEditorDown", "Split Horizontal")
-	vsc("<leader>wc", "workbench.action.closeActiveEditor", "Close Window")
-	vsc("<leader>wo", "workbench.action.closeEditorsInOtherGroups", "Close Others")
+	vsc_normal("<leader>wv", "workbench.action.splitEditorRight", "Split Vertical")
+	vsc_normal("<leader>ws", "workbench.action.splitEditorDown", "Split Horizontal")
+	vsc_normal("<leader>wc", "workbench.action.closeActiveEditor", "Close Window")
+	vsc_normal("<leader>wo", "workbench.action.closeEditorsInOtherGroups", "Close Others")
 
 	-- Window Navigation (Focus Groups)
-	vsc("<leader>wh", "workbench.action.focusLeftGroup", "Focus Left")
-	vsc("<leader>wj", "workbench.action.focusBelowGroup", "Focus Below")
-	vsc("<leader>wk", "workbench.action.focusAboveGroup", "Focus Above")
-	vsc("<leader>wl", "workbench.action.focusRightGroup", "Focus Right")
+	vsc_normal("<leader>wh", "workbench.action.focusLeftGroup", "Focus Left")
+	vsc_normal("<leader>wj", "workbench.action.focusBelowGroup", "Focus Below")
+	vsc_normal("<leader>wk", "workbench.action.focusAboveGroup", "Focus Above")
+	vsc_normal("<leader>wl", "workbench.action.focusRightGroup", "Focus Right")
 
 	-- Lsp
-	vsc("<leader>lf", "editor.action.formatDocument", "Format Document")
-	-- TODO: Add more LSP actions
+	vsc_normal("<leader>lf", "editor.action.formatDocument", "Format Document")
+	vsc_normal("<leader>uf", "workbench.view.explorer", "Toggle File Explorer")
+	vsc_insert("<C-\\>", "workbench.action.terminal.toggleTerminal")
+	vsc_mode({ "n", "i" }, "<leader>uc", "workbench.action.chat.openagent")
 end
