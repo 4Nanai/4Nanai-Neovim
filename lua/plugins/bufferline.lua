8437e55dee6206f649fcf2e3c0a5db06ca82e116
4Nanai <103087652@qq.com>
2026-04-02 03:38:51 -0400

    feat(plugins): add vim-matchup plugin and update tokyonight highlights

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -31,8 +31,8 @@ return {
 		{ "<leader>bl", ":BufferLineCycleNext<CR>", silent = true, desc = "Buffer Right" },
 		{ "<leader>bp", ":BufferLinePick<CR>", silent = true, desc = "Buffer Pick" },
 		{ "<leader>bd", ":bdelete<CR>", silent = true, desc = "Buffer Delete" },
-        { "<leader><Left>", ":BufferLineMovePrev<CR>", silent = true, desc = "Buffer Move Left" },
-        { "<leader><Right>", ":BufferLineMoveNext<CR>", silent = true, desc = "Buffer Move Right" },
+        { "<leader>by", ":BufferLineMovePrev<CR>", silent = true, desc = "Buffer Move Left" },
+        { "<leader>bo", ":BufferLineMoveNext<CR>", silent = true, desc = "Buffer Move Right" },
 	},
 	lazy = false,
 }
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,7 +1,20 @@
 return {
     "folke/tokyonight.nvim",
     opts = {
-        style = "moon"
+        style = "moon",
+        on_highlights = function (hl, c)
+            local LineNrColor = "#8389a3"
+
+            hl.LineNr = {
+                fg = "#8389a3",
+            }
+            hl.LineNrAbove = {
+                fg = LineNrColor,
+            }
+            hl.LineNrBelow = {
+                fg = LineNrColor,
+            }
+        end,
     },
     config = function(_, opts)
         require("tokyonight").setup(opts)
