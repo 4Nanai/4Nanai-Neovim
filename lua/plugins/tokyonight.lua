091aa2307dfc61ac8e6a6ea1f63c20a0038febbd
4Nanai <103087652@qq.com>
2026-04-11 01:40:42 -0400

    chore: change color for diagnosed unnecessary code

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -15,6 +15,9 @@ return {
             hl.LineNrBelow = {
                 fg = LineNrColor,
             }
+            hl.DiagnosticUnnecessary = {
+                fg = "#828bb8",
+            }
         end,
     },
     config = function(_, opts)
